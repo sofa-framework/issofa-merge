sofa-framework/issofa-merge

commit 9018ea44dc832a989c54ff1da23ff9591487fd14

SparseLDLSolver output more info to files if asked + factorization is recomputed if orderingMode is changed

modules/SofaSparseSolver/SparseLDLSolverImpl.h
@@ -44,9 +44,10 @@ namespace linearsolver
 
 //defaut structure for a LDL factorization
 template<class VecInt,class VecReal>
-class SpaseLDLImplInvertData : public MatrixInvertData {
+class SparseLDLImplInvertData : public MatrixInvertData {
 public :
     int n, P_nnz, L_nnz;
+    int orderingMode;
     VecInt P_rowind,P_colptr,L_rowind,L_colptr,LT_rowind,LT_colptr;
     VecInt perm, invperm;
     VecReal L_values,LT_values,invD;
@@ -171,7 +172,7 @@ protected :
     SparseLDLSolverImpl() : Inherit() {}
 
     template<class VecInt,class VecReal>
-    void solve_cpu(Real * x,const Real * b,SpaseLDLImplInvertData<VecInt,VecReal> * data) {
+    void solve_cpu(Real * x,const Real * b,SparseLDLImplInvertData<VecInt,VecReal> * data) {
         int n = data->n;
         const Real * invD = &data->invD[0];
         const int * perm = &data->perm[0];
@@ -283,8 +284,9 @@ protected :
     }
 
     template<class VecInt,class VecReal>
-    void factorize(int n,int * M_colptr, int * M_rowind, Real * M_values, SpaseLDLImplInvertData<VecInt,VecReal> * data) {
-        data->new_factorization_needed = data->P_colptr.size() == 0 || data->P_rowind.size() == 0 || CSPARSE_need_symbolic_factorization(n, M_colptr, M_rowind, data->n, (int *) &data->P_colptr[0],(int *) &data->P_rowind[0]);
+    void factorize(int n,int * M_colptr, int * M_rowind, Real * M_values, SparseLDLImplInvertData<VecInt,VecReal> * data) {
+        const int orderingMode = this->d_orderingMode.getValue();
+        bool new_factorization_needed = data->P_colptr.size() == 0 || data->P_rowind.size() == 0 || data->orderingMode != orderingMode
 
         data->n = n;
         data->P_nnz = M_colptr[data->n];
@@ -293,6 +295,7 @@ protected :
 
         // we test if the matrix has the same struct as previous factorized matrix
         if (data->new_factorization_needed) {
+            data->orderingMode = orderingMode;
             data->perm.clear();data->perm.fastResize(data->n);
             data->invperm.clear();data->invperm.fastResize(data->n);
             data->invD.clear();data->invD.fastResize(data->n);
@@ -305,7 +308,7 @@ protected :
             memcpy(&data->P_rowind[0],M_rowind,data->P_nnz * sizeof(int));
 
             //ordering function
-            LDL_ordering(data->n,M_colptr,M_rowind,&data->perm[0],&data->invperm[0]);
+            LDL_ordering(data->n,M_colptr,M_rowind,&data->perm[0],&data->invperm[0], orderingMode);
 
             data->Parent.clear();
             data->Parent.resize(data->n);
@@ -371,7 +374,7 @@ protected :
     }
 
     helper::vector<Real> Tmp;
-protected : //the folowing variables are used during the factorization they canno be used in the main thread !
+protected : //the following variables are used during the factorization they cannot be used in the main thread !
     helper::vector<int> xadj,adj,t_xadj,t_adj;
     helper::vector<Real> Y;
     helper::vector<int> Lnz,Flag,Pattern;

modules/SofaSparseSolver/SparseLDLSolver.cpp
@@ -38,11 +38,11 @@ namespace linearsolver
 
 SOFA_DECL_CLASS(SparseLDLSolver)
 
-int SparseLDLSolverClass = core::RegisterObject("Direct linear solver based on Sparse LDL^T factorization, implemented with the CSPARSE library")
+int SparseLDLSolverClass = core::RegisterObject("Direct linear solver based on Sparse LDL^T factorization")
         .add< SparseLDLSolver< CompressedRowSparseMatrix<double>,FullVector<double> > >(true)
-        .add< SparseLDLSolver< CompressedRowSparseMatrix<defaulttype::Mat<3,3,double> >,FullVector<double> > >(true)
-        .add< SparseLDLSolver< CompressedRowSparseMatrix<float>,FullVector<float> > >(true)
-        .add< SparseLDLSolver< CompressedRowSparseMatrix<defaulttype::Mat<3,3,float> >,FullVector<float> > >(true)
+        .add< SparseLDLSolver< CompressedRowSparseMatrix<defaulttype::Mat<3,3,double> >,FullVector<double> > >()
+        .add< SparseLDLSolver< CompressedRowSparseMatrix<float>,FullVector<float> > >()
+        .add< SparseLDLSolver< CompressedRowSparseMatrix<defaulttype::Mat<3,3,float> >,FullVector<float> > >()
         ;
 
 template class SOFA_SPARSE_SOLVER_API SparseLDLSolver< CompressedRowSparseMatrix<double>,FullVector<double> >;

modules/SofaSparseSolver/SparseLDLSolver.h
@@ -58,7 +58,7 @@ public :
     typedef sofa::component::linearsolver::SparseLDLSolverImpl<TMatrix,TVector,TThreadManager> Inherit;
     typedef typename Inherit::ResMatrixType ResMatrixType;
     typedef typename Inherit::JMatrixType JMatrixType;
-    typedef SpaseLDLImplInvertData<helper::vector<int>, helper::vector<Real> > InvertData;
+    typedef SparseLDLImplInvertData<helper::vector<int>, helper::vector<Real> > InvertData;
 
     void solve (Matrix& M, Vector& x, Vector& b);
     void invert(Matrix& M);

modules/SofaSparseSolver/SparseLDLSolver.inl
@@ -59,7 +59,7 @@ void SparseLDLSolver<TMatrix,TVector,TThreadManager>::invert(Matrix& M) {
     if (f_saveMatrixToFile.getValue()) {
         std::ofstream f;
         char name[100];
-        sprintf(name, "matrixInLDLInvert_%04d.txt", numStep);
+        sprintf(name, "LDL_matrix_%04d.txt", numStep);
         f.open(name);
         f << M;
         f.close();
@@ -74,7 +74,95 @@ void SparseLDLSolver<TMatrix,TVector,TThreadManager>::invert(Matrix& M) {
     int * M_rowind = (int *) &Mfiltered.getColsIndex()[0];
     Real * M_values = (Real *) &Mfiltered.getColsValue()[0];
 
-    Inherit::factorize(n,M_colptr,M_rowind,M_values,(InvertData *) this->getMatrixInvertData(&M));
+    InvertData * data = (InvertData *) this->getMatrixInvertData(&M);
+    Inherit::factorize(n,M_colptr,M_rowind,M_values,data);
+
+    if (f_saveMatrixToFile.getValue()) {
+        { // L
+            std::ofstream f;
+            char name[100];
+            sprintf(name, "LDL_L_%04d.txt", numStep);
+            f.open(name);
+            f << data->n << std::endl;
+            f << data->L_rowind << std::endl;
+            f << data->L_colptr << std::endl;
+            f << data->L_values << std::endl;
+            f << std::endl;
+            f.close();
+        }
+        { // Dinv
+            std::ofstream f;
+            char name[100];
+            sprintf(name, "LDL_Dinv_%04d.txt", numStep);
+            f.open(name);
+            f << data->n << std::endl;
+            f << data->invD << std::endl;
+            f << std::endl;
+            f.close();
+        }
+        { // Perm
+            std::ofstream f;
+            char name[100];
+            sprintf(name, "LDL_Perm_%04d.txt", numStep);
+            f.open(name);
+            f << data->perm << std::endl;
+            f << data->invperm << std::endl;
+            f << std::endl;
+            f.close();
+        }
+        { // M pattern
+            std::ofstream f;
+            char name[100];
+            sprintf(name, "LDL_Mpattern_%04d.txt", numStep);
+            f.open(name);
+            const int nbRow = Mfiltered.rowSize();
+            const int nbCol = Mfiltered.colSize();
+            for (int r = 0; r < nbRow; ++r)
+            {
+                const int rBegin = M_colptr[r];
+                const int rEnd = M_colptr[r+1];
+                int c = 0;
+                f << '[';
+                for (int it = rBegin; it != rEnd; ++it)
+                {
+                    int col = M_rowind[it];
+                    for (;c < col; ++c) f << ' ';
+                    f << 'x';
+                }
+                for (;c < nbCol; ++c) f << ' ';
+                f << ']';
+                f << std::endl;
+            }
+            f << std::endl;
+            f.close();
+        }
+        { // L pattern
+            std::ofstream f;
+            char name[100];
+            sprintf(name, "LDL_Lpattern_%04d.txt", numStep);
+            f.open(name);
+            const int nbRow = Mfiltered.rowSize();
+            const int nbCol = Mfiltered.colSize();
+            for (int r = 0; r < nbRow; ++r)
+            {
+                const int rBegin = data->L_colptr[r];
+                const int rEnd = data->L_colptr[r+1];
+                int c = 0;
+                f << '[';
+                for (int it = rBegin; it != rEnd; ++it)
+                {
+                    int col = data->L_rowind[it];
+                    for (;c < col; ++c) f << ' ';
+                    f << 'x';
+                }
+                for (;c < nbCol; ++c) f << ' ';
+                f << ']';
+                f << std::endl;
+            }
+            f << std::endl;
+            f.close();
+        }
+    }
 
     numStep++;
 }

commit eaf6a9595c624e3cb8a2b81273e470a9465b7344

FIX : Fix problems introduced by commit 9018ea44dc832a989c54ff1da23ff9591487fd14 with the merge

SofaKernel/framework/sofa/simulation/MechanicalVisitor.cpp
@@ -1405,6 +1405,7 @@ Visitor::Result MechanicalWriteLMConstraint::fwdConstraintSet(simulation::Node* 
 Visitor::Result MechanicalAccumulateConstraint::fwdConstraintSet(simulation::Node* node, core::behavior::BaseConstraintSet* c)
 {
     ctime_t t0 = begin(node, c);
+    c->setConstraintId(contactId);
     c->buildConstraintMatrix(cparams, res, contactId);
     end(node, c, t0);
     return RESULT_CONTINUE;

modules/SofaSparseSolver/SparseLDLSolverImpl.h
@@ -167,9 +167,14 @@ public:
     typedef TThreadManager ThreadManager;
     typedef typename TMatrix::Real Real;
 
+    Data<int> d_orderingMode;
+
 protected :
 
-    SparseLDLSolverImpl() : Inherit() {}
+    SparseLDLSolverImpl()
+    : Inherit(),
+      d_orderingMode(initData(&d_orderingMode,1,"orderingMode", "Permutation computation algorithm: 0 = disabled, 1 = METIS (available)"))
+    {}
 
     template<class VecInt,class VecReal>
     void solve_cpu(Real * x,const Real * b,SparseLDLImplInvertData<VecInt,VecReal> * data) {
@@ -286,7 +291,8 @@ protected :
     template<class VecInt,class VecReal>
     void factorize(int n,int * M_colptr, int * M_rowind, Real * M_values, SparseLDLImplInvertData<VecInt,VecReal> * data) {
         const int orderingMode = this->d_orderingMode.getValue();
-        bool new_factorization_needed = data->P_colptr.size() == 0 || data->P_rowind.size() == 0 || data->orderingMode != orderingMode
+        data->new_factorization_needed = data->P_colptr.size() == 0 || data->P_rowind.size() == 0 || data->orderingMode != orderingMode
+                || CSPARSE_need_symbolic_factorization(n, M_colptr, M_rowind, data->n, (int *) &data->P_colptr[0],(int *) &data->P_rowind[0]);
 
         data->n = n;
         data->P_nnz = M_colptr[data->n];
@@ -308,7 +314,7 @@ protected :
             memcpy(&data->P_rowind[0],M_rowind,data->P_nnz * sizeof(int));
 
             //ordering function
-            LDL_ordering(data->n,M_colptr,M_rowind,&data->perm[0],&data->invperm[0], orderingMode);
+            LDL_ordering(data->n,M_colptr,M_rowind,&data->perm[0],&data->invperm[0]);
 
             data->Parent.clear();
             data->Parent.resize(data->n);