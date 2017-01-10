InSimo/ISSofa

UPDATE: display node name in addition of node type on xml node creation failure for easier debugging

modules/sofa/simulation/common/xml/ObjectElement.cpp
@@ -83,7 +83,7 @@ bool ObjectElement::initNode()
         obj = Factory::CreateObject(this->getType(), this);
     if (obj == NULL)
     {
-        getParent()->logWarning(std::string("Object type \"" + getType() + "\" creation Failed" ));
+        getParent()->logWarning(std::string("Object " + getName() + "(" + getType() + ") creation Failed" ));
         return false;
     }
     setObject(obj);

sofa-framework/issofa-merge

SofaKernel/modules/SofaSimulationCommon/xml/ObjectElement.cpp
@@ -83,7 +83,7 @@ bool ObjectElement::initNode()
         obj = Factory::CreateObject(this->getType(), this);
     if (obj == NULL)
     {
-        getParent()->logError(std::string("Object type \"" + getType() + "\" creation Failed" ));
+        getParent()->logError(std::string("Object " + getName() + "(" + getType() + ") creation Failed" ));
         return false;
     }
     setObject(obj);