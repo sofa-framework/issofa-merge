sofa-framework/issofa-merge

commit eb42047c1ae0758754d5cc9a36ff3d969f7b1a9b

BUILD UPDATE: csparse, SofaSparseSolver library
Conflicts:
	SofaGeneral/CMakeLists.txt

modules/SofaSparseSolver/SparseCholeskySolver.h
@@ -32,9 +32,10 @@
 #include <SofaBaseLinearSolver/FullMatrix.h>
 #include <SofaBaseLinearSolver/SparseMatrix.h>
 #include <SofaBaseLinearSolver/CompressedRowSparseMatrix.h>
+#include <sofa/SofaGeneral.h>
 #include <sofa/helper/map.h>
 #include <math.h>
-#include <csparse.h>
+#include <csparse/csparse.h>
 
 namespace sofa
 {

modules/SofaSparseSolver/SparseLUSolver.h
@@ -33,7 +33,7 @@
 #include <SofaBaseLinearSolver/FullMatrix.h>
 #include <sofa/helper/map.h>
 #include <math.h>
-#include <csparse.h>
+#include <csparse/csparse.h>
 
 namespace sofa
 {

modules/SofaComponentGeneral/initComponentGeneral.cpp
@@ -50,9 +50,9 @@
 #ifdef SOFA_HAVE_NEWMAT
 #include <SofaDenseSolver/initDenseSolver.h>
 #endif
-#ifdef SOFA_HAVE_CSPARSE
+
 #include <SofaSparseSolver/initSparseSolver.h>
-#endif
+
 #ifdef SOFA_HAVE_TAUCS
 #include <SofaTaucsSolver/initTaucsSolver.h>
 #endif
@@ -102,9 +102,7 @@ void initComponentGeneral()
 #ifdef SOFA_HAVE_NEWMAT
     initDenseSolver();
 #endif
-#ifdef SOFA_HAVE_CSPARSE
     initSparseSolver();
-#endif
 #ifdef SOFA_HAVE_TAUCS
     initTaucsSolver();
 #endif

modules/SofaSparseSolver/initSparseSolver.cpp
@@ -25,7 +25,6 @@
 #include <sofa/helper/system/config.h>
 #include <SofaSparseSolver/initSparseSolver.h>
 
-
 namespace sofa
 {
 
@@ -42,8 +41,9 @@ void initSparseSolver()
     }
 }
 
-#ifdef SOFA_HAVE_CSPARSE
 SOFA_LINK_CLASS(PrecomputedLinearSolver)
+
+#ifdef SOFA_HAVE_CSPARSE
 SOFA_LINK_CLASS(SparseCholeskySolver)
 SOFA_LINK_CLASS(SparseLUSolver)
 #endif

commit c3103736e4de7d471ab8b680eb7bb7f2d16dfb35

FIX: fix problems of compilation with csparse introduced by commit : eb42047c1ae0758754d5cc9a36ff3d969f7b1a9b

modules/SofaSparseSolver/SparseCholeskySolver.h
@@ -32,10 +32,9 @@
 #include <SofaBaseLinearSolver/FullMatrix.h>
 #include <SofaBaseLinearSolver/SparseMatrix.h>
 #include <SofaBaseLinearSolver/CompressedRowSparseMatrix.h>
-#include <sofa/SofaGeneral.h>
 #include <sofa/helper/map.h>
 #include <math.h>
-#include <csparse/csparse.h>
+#include <csparse.h>
 
 namespace sofa
 {

modules/SofaSparseSolver/SparseLUSolver.h
@@ -33,7 +33,7 @@
 #include <SofaBaseLinearSolver/FullMatrix.h>
 #include <sofa/helper/map.h>
 #include <math.h>
-#include <csparse/csparse.h>
+#include <csparse.h>
 
 namespace sofa
 {