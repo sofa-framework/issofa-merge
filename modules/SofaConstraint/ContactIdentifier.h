InSimo/ISSofa

FIX: compilation of SofaConstraint

modules/SofaConstraint/ContactIdentifier.h
@@ -27,7 +27,7 @@
 
 #include <sofa/core/collision/DetectionOutput.h>
 
-#include <sofa/component/initConstraint.h>
+#include <sofa/SofaGeneral.h>
 
 #include <list>
 

sofa-framework/issofa-merge

modules/SofaConstraint/ContactIdentifier.h
@@ -27,7 +27,7 @@
 
 #include <sofa/core/collision/DetectionOutput.h>
 
-#include <sofa/component/initConstraint.h>
+#include <SofaConstraint/initConstraint.h>
 
 #include <list>
 